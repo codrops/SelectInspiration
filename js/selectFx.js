d68166d8e7fad2573384621dc2f9124e7350a4b7
Glen <glen.84@gmail.com>
2016-07-20 15:57:15 +0200

    Better way to get selected option

--- js/selectFx.js
+++ js/selectFx.js
@@ -66,7 +66,7 @@
 	SelectFx.prototype._init = function() {
 		// check if we are using a placeholder for the native select box
 		// we assume the placeholder is disabled and selected by default
-		var selectedOpt = this.el.querySelector( 'option[selected]' );
+		var selectedOpt = this.el.options[this.el.selectedIndex];
 		this.hasDefaultPlaceholder = selectedOpt && selectedOpt.disabled;
 
 		// get selected option (either the first option with attr selected or just the first option)
